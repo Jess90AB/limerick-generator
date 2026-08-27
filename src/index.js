a3089f441c1d08e011cdcf30dfe420afcb327463
Jess90AB <82506428+Jess90AB@users.noreply.github.com>
2026-08-27 14:56:52 +0800

    Added basic form html & typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateLimerick(event) {
+  event.preventDefault();
 
+  let limerickElement = document.querySelector("#limerick");
+
+  new Typewriter("#limerick", {
+    strings: ["A dozen, a gross, and a score,"],
+    autoStart: true,
+    delay: 50,
+    cursor: "",
+  });
+}
+
+let limerickForm = document.querySelector("#limerick-form");
+limerickForm.addEventListener("submit", generateLimerick);
