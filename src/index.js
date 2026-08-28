2cd5edad647c506ce56ccc760681d8fb39ff46f8
Jess90AB <82506428+Jess90AB@users.noreply.github.com>
2026-08-28 12:26:06 +0800

    Fixed bug in js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
 function displayLimerick(response) {
-  console.log("limerick generated");
-
   new Typewriter("#limerick", {
     strings: [response.data.answer],
     autoStart: true,
     delay: 50,
     cursor: "",
-    isDeleting: false,
   });
 }
 function generateLimerick(event) {
   event.preventDefault();
 
+  alert(
+    "Your limerick is being generated. Please get ready to have your socks blown off.",
+  );
+
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "465af09aod8c2624e9ft9e3093eb0ebb";
   let prompt = `Generate a limerick about ${instructionsInput.value}`;
   let context =
-    "You are a limerick generator. A limerick is a humorous poem consisting of five lines. The first, second, and fifth lines rhyme with each other, while the third and fourth lines rhyme with each other. The first, second, and fifth lines have three metrical feet, while the third and fourth lines have two metrical feet. Please generate a limerick based on the user instructions. Please format the limerick in basic HTML.";
+    "You are a limerick generator. A limerick is a humorous poem consisting of five lines. The first, second, and fifth lines rhyme with each other, while the third and fourth lines rhyme with each other. The first, second, and fifth lines have three metrical feet, while the third and fourth lines have two metrical feet. Please generate a limerick based on the user instructions. Please put use <br/> after each line of the limerick. Please do not include any additional text or explanation. Please do not include any hashtags or emojis.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
-
-  console.log("Generating limerick:");
-  console.log(`Prompt: ${prompt}`);
-  console.log(`Context: ${context}`);
 
   axios.get(apiURL).then(displayLimerick);
 }
